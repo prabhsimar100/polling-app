ed18f9c6850c30dc933572a1a4377bec0fdbe445
Prajjwal Kumar <prajjwalkr1999@gmail.com>
2021-01-16 19:16:36 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { withFirebase } from "./components/Firebase";
+import { withAuthentication } from "./components/Session";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//components
+import Navbar from "./components/Navbar";
+
+//Pages
+import home from "./pages/home";
+import signup from "./pages/signup";
+import login from "./pages/login";
+import profile from "./pages/profile";
+// import landing from "./pages/landing";
+import forpass from "./pages/forpass"
+import passchange from "./pages/passchange"
+import completed from "./pages/completed";
+import upload from "./pages/upload";
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#c158dc",
+      main: "#8e24aa",
+      dark: "#5c007a",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ffc4ff",
+      main: "#ce93d8",
+      dark: "#9c64a6",
+      contrastText: "#fff",
+    },
+  },
+});
+
+class App extends Component {
+  componentDidUnmount(){
+    this.props.firebase.doSignOut();
+  }
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={landing} />
+                <Route exact path="/signup" component={signup} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/profile" component={profile} />
+                <Route exact path="/forpass" component={forpass} />
+                <Route exact path="/passchange" component={passchange} />
+                <Route exact path="/create" component={create} />
+                <Route exact path="/running" component={running} />
+                <Route exact path="/completed" component={completed} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
-export default App;
+export default withFirebase(withAuthentication(App));
