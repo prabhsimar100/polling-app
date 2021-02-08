5b00dab598db6075877ac4be233a7d570dda38d6
Prajjwal Kumar <prajjwalkr1999@gmail.com>
2021-02-09 02:07:59 +0530

    Landing

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,3 +1,4 @@
+import { QuestionAnswer } from "@material-ui/icons";
 import React, { Component } from "react";
 // import Candidate from "../components/Candidate";
 
@@ -17,34 +18,38 @@ class landing extends Component
 
   componentDidMount()
   {
-
     this.props.firebase.db
     .collection("surveys")
     .get()
     .then((querySnapshot) => {
       var ar = []
-      var arr = []
-
       querySnapshot.forEach((doc) => {
-        // console.log(doc.id);
+        var arr = []
         this.props.firebase.db.collection("surveys").doc(doc.id)
         .collection("options")
         .get()
         .then((querySnapshot)=>{
           querySnapshot.forEach((doc)=>{            
-            arr.push(doc.data());
-            // console.log(doc);
+            arr.push(doc);
           })
         })
         .catch((error) => {
           console.log(error);
-        })  
-        console.log(arr);
-        // ar.push(doc)
-      });
+        })
+        const surveyTemp = {
+          questions : doc,
+          options : arr,
+        }  
+        ar.push(surveyTemp);
+        // console.log(ar);
+      })
       this.setState({surveys : ar})
-      // console.log(this.state.surveys[0].data()); 
-      // console.log(this.state.surveys[0].id); 
+      console.log(this.state.surveys[0].options[0]);
+      // console.log(this.state.surveys[0].options);
+      // for(var i=0;i<2;i++){
+      //   console.log("HELOO");
+      //   console.log(this.state.surveys[0].options[i]);
+      // }
     });
   }
 
